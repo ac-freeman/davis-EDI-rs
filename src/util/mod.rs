44c43bf9ec04aebbef8c72f4519fe699d24aa5fa
Andrew Freeman <andrew_freeman@baylor.edu>
2024-09-09 15:58:34 -0500

    Faster conversion and bump crate version

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,10 @@
+use std::arch::x86_64::{_mm_loadu_pd, _mm_storeu_pd};
 use nalgebra::{Dyn, OMatrix};
 use opencv::core::{Mat, MatExprTraitConst, MatTrait, CV_64F};
+use std::arch::x86_64::*;
+use std::arch::x86_64::{_mm256_loadu_pd, _mm256_storeu_pd};
+
+
 
 pub(crate) mod event_adder;
 pub mod reconstructor;
@@ -10,9 +15,24 @@ fn omatrix_to_mat(omatrix: &OMatrix<f64, Dyn, Dyn>) -> Mat {
     let cols = omatrix.ncols() as i32;
     let mut mat = Mat::zeros(rows, cols, CV_64F).unwrap().to_mat().unwrap();
 
-    for i in 0..rows {
-        for j in 0..cols {
-            *mat.at_2d_mut::<f64>(i, j).unwrap() = omatrix[(i as usize, j as usize)];
+    unsafe {
+        let mat_ptr = mat.ptr_mut(0).unwrap() as *mut f64;
+        let omatrix_ptr = omatrix.transpose().as_slice().as_ptr();
+
+        let len = (rows * cols) as isize;
+        let simd_width = 4; // Number of f64 values processed per SIMD operation
+
+        let mut i = 0;
+        while i <= len - simd_width {
+            let data = _mm256_loadu_pd(omatrix_ptr.offset(i));
+            _mm256_storeu_pd(mat_ptr.offset(i), data);
+            i += simd_width;
+        }
+
+        // Handle remaining elements
+        while i < len {
+            *mat_ptr.offset(i) = *omatrix_ptr.offset(i);
+            i += 1;
         }
     }
 
